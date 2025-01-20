e6ca5d7e1acb2ea81eaa61033fc1e8b2e0904dd5
ETairkhan <trouzerop@gmail.com>
2025-01-20 11:10:43 +0500

    an example for our project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import controllers.UserController;
+import controllers.interfaces.IUserController;
+import data.PostgresDB;
+import data.interfaceces.IDB;
+import repositories.UserRepository;
+import repositories.interfaces.IUserRepository;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "Tair", "0000", "Tair2");
+        IUserRepository repo = new UserRepository(db);
+        IUserController controller = new UserController(repo);
+        MyApplication app = new MyApplication(controller);
+        app.start();
+
+        db.close();
     }
 }
