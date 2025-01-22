a19fc75aa02396afc083c053f8d492ae27d6b068
ETairkhan <trouzerop@gmail.com>
2025-01-22 15:17:02 +0500

    basic

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,7 @@ import repositories.interfaces.IUserRepository;
 
 public class Main {
     public static void main(String[] args) {
-
-        // We have to remake this one and others to our Bank project 'ZEBT'
-        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "Tair", "0000", "Tair2");
+        IDB db = new PostgresDB("jdbc:postgresql://localhost:5432", "postgres", "0000", "simpledb");
         IUserRepository repo = new UserRepository(db);
         IUserController controller = new UserController(repo);
         MyApplication app = new MyApplication(controller);
